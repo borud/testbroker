a93e9cf03185094f32b9ec9e83ed25492101f727
Bjorn Borud <borud@lab5e.com>
2022-10-13 11:36:49 +0200

    use internal logging and packet dump

--- main.go
+++ main.go
@@ -22,6 +22,8 @@ func main() {
 
 	mqttConfig := config.DefaultConfig()
 	mqttConfig.MQTT.AllowZeroLenClientID = false
+	mqttConfig.Logging = true
+	mqttConfig.DumpPacket = true
 
 	listenSocket, err := net.Listen("tcp", listenAddr)
 	if err != nil {
@@ -33,41 +35,28 @@ func main() {
 		lmqtt.WithConfig(mqttConfig),
 		lmqtt.WithHook(lmqtt.Hooks{
 			OnAccept: func(ctx context.Context, conn net.Conn) bool {
-				log.Printf("\nOnAccept: conn = [%+v]", conn)
 				return true
 			},
-			OnStop: func(ctx context.Context) {
-				log.Printf("\nOnStop")
-			},
+			OnStop: func(ctx context.Context) {},
 			OnSubscribe: func(ctx context.Context, client lmqtt.Client, req *lmqtt.SubscribeRequest) error {
-				log.Printf("\nOnSubscribe: client=[%+v] req=[%+v]", client, req)
 				return nil
 			},
-			OnSubscribed: func(ctx context.Context, client lmqtt.Client, subscription *entities.Subscription) {
-				log.Printf("\nOnSubscribed: client=[%+v] subscrioption=[%+v]", client, subscription)
-			},
+			OnSubscribed: func(ctx context.Context, client lmqtt.Client, subscription *entities.Subscription) {},
 			OnUnsubscribe: func(ctx context.Context, client lmqtt.Client, req *lmqtt.UnsubscribeRequest) error {
-				log.Printf("\nOnUnsubscribe: client=[%+v] req=[%+v]", client, req)
 				return nil
 			},
-			OnUnsubscribed: func(ctx context.Context, client lmqtt.Client, topicName string) {
-				log.Printf("\nOnUnsubscribed: client=[%+v] topicName=[%s]", client, topicName)
-			},
+			OnUnsubscribed: func(ctx context.Context, client lmqtt.Client, topicName string) {},
 			OnMsgArrived: func(ctx context.Context, client lmqtt.Client, req *lmqtt.MsgArrivedRequest) error {
-				log.Printf("\nOnMsgArrived: client=[%+v] req=[%+v]", client, req)
 				return nil
 			},
 			OnBasicAuth: func(ctx context.Context, client lmqtt.Client, req *lmqtt.ConnectRequest) error {
-				log.Printf("\nOnBasicAuth: client=[%+v] req=[%+v]", client, req)
 				return nil
 			},
 
 			OnEnhancedAuth: func(ctx context.Context, client lmqtt.Client, req *lmqtt.ConnectRequest) (*lmqtt.EnhancedAuthResponse, error) {
-				log.Printf("\nOnEnhancedAuth: client=[%+v] req=[%+v]", client, req)
 				return &lmqtt.EnhancedAuthResponse{
 					Continue: false,
 					OnAuth: func(ctx context.Context, client lmqtt.Client, req *lmqtt.AuthRequest) (*lmqtt.AuthResponse, error) {
-						log.Printf("\nOnEnhancedAuth -> OnAuth client=[%+v] req=[%+v]", client, req)
 						return &lmqtt.AuthResponse{
 							Continue: false,
 							AuthData: []byte{},
@@ -78,43 +67,23 @@ func main() {
 			},
 
 			OnReAuth: func(ctx context.Context, client lmqtt.Client, auth *packets.Auth) (*lmqtt.AuthResponse, error) {
-				log.Printf("\nOnReAuth: client=[%+v] auth=[%+v]", client, auth)
-
 				// what would be the best response for debug?
 				return &lmqtt.AuthResponse{
 					Continue: false,
 					AuthData: []byte{},
 				}, err
 			},
-			OnConnected: func(ctx context.Context, client lmqtt.Client) {
-				log.Printf("\nOnConnected: client=[%+v]", client)
-			},
-			OnSessionCreated: func(ctx context.Context, client lmqtt.Client) {
-				log.Printf("\nOnSessionCreated: client=[%+v]", client)
-			},
-			OnSessionResumed: func(ctx context.Context, client lmqtt.Client) {
-				log.Printf("\nOnSessionResumed: client=[%+v]", client)
-			},
-			OnSessionTerminated: func(ctx context.Context, clientID string, reason lmqtt.SessionTerminatedReason) {
-				log.Printf("\nOnSessionTerminated: clientID=[%s] reason=[%+v]", clientID, reason)
-			},
-			OnDelivered: func(ctx context.Context, client lmqtt.Client, msg *entities.Message) {
-				log.Printf("\nOnDelivered: client=[%+v] msg=[%+v]", client, msg)
-			},
-			OnClosed: func(ctx context.Context, client lmqtt.Client, err error) {
-				log.Printf("\nOnClosed: client=[%+v] err=[%+v]", client, err)
-			},
-			OnMsgDropped: func(ctx context.Context, clientID string, msg *entities.Message, err error) {
-				log.Printf("\nOnMsgDropped: clientID=[%s] msg=[%+v] err=[%+v]", clientID, msg, err)
-			},
-			OnWillPublish: func(ctx context.Context, clientID string, req *lmqtt.WillMsgRequest) {
-				log.Printf("\nOnWillPublish: clientID=[%s] req=[%+v]", clientID, req)
-			},
-			OnWillPublished: func(ctx context.Context, clientID string, msg *entities.Message) {
-				log.Printf("\nOnWillPublished: clientID=[%s] msg=[%+v]", clientID, msg)
-			},
+			OnConnected:         func(ctx context.Context, client lmqtt.Client) {},
+			OnSessionCreated:    func(ctx context.Context, client lmqtt.Client) {},
+			OnSessionResumed:    func(ctx context.Context, client lmqtt.Client) {},
+			OnSessionTerminated: func(ctx context.Context, clientID string, reason lmqtt.SessionTerminatedReason) {},
+
+			OnDelivered:     func(ctx context.Context, client lmqtt.Client, msg *entities.Message) {},
+			OnClosed:        func(ctx context.Context, client lmqtt.Client, err error) {},
+			OnMsgDropped:    func(ctx context.Context, clientID string, msg *entities.Message, err error) {},
+			OnWillPublish:   func(ctx context.Context, clientID string, req *lmqtt.WillMsgRequest) {},
+			OnWillPublished: func(ctx context.Context, clientID string, msg *entities.Message) {},
 			OnPublish: func(ctx context.Context, client lmqtt.Client, msg *entities.Message) bool {
-				log.Printf("\nOnPublish: client=[%+v] msg=[%+v]", client, msg)
 				return true
 			},
 		}))
